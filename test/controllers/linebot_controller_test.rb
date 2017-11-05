9487c6a5b59ffb84cf8eee361bfb5fdcaa2014e5
岡本勇太 <yuta_okamoto428@yahoo.co.jp>
2017-11-06 00:53:06 +0900

    送信用slim、css追加

--- test/controllers/linebot_controller_test.rb
+++ test/controllers/linebot_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class LinebotControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get callback" do
+    get linebot_callback_url
+    assert_response :success
+  end
+
+  test "should get push" do
+    get linebot_push_url
+    assert_response :success
+  end
+
 end
